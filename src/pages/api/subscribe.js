c50dc0a93d02777cc68503f961dff9042812bfe5
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-07-09 13:43:20 +0500

    send mail to notify me after someone subscribe

--- src/pages/api/subscribe.js
+++ src/pages/api/subscribe.js
@@ -1,6 +1,6 @@
 import connectDB from "@/middlewares/db";
 import Newsletter from "@/models/Newsletter";
-
+const nodemailer = require('nodemailer');
 const handler = async (req, response) => {
     try {
         const existingSubscriber = await Newsletter.findOne({email: req.body.email});
@@ -9,6 +9,45 @@ const handler = async (req, response) => {
 
         }
         const newSubscriber = await Newsletter.create({ name: req.body.name, email: req.body.email, project: "buildspace-project"});
+
+
+        try {
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: process.env.NODEMAILER_EMAIL,
+                  pass: process.env.EMAIL_PASSWORD
+                }
+              });
+              
+              var mailOptions = {
+                from: process.env.NODEMAILER_EMAIL,
+                to: 'hussnainahmad1606@gmail.com',
+                subject: `New Subscriber - Buildspace`,
+                html: `
+                <p>Hi, you got a new Subscriber</p>
+                <p>Name: ${req.body.name}</p>
+                <p>Email: ${req.body.email}</p>
+                `
+                
+              };
+              
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+              });
+             
+        
+             
+        
+             
+          } catch (err) {
+              console.error('Error while sending mail', err);
+              
+          }
         
         return response.status(201).json({type: "success", message: "You have been added to the wishlist!"});
     } catch (err) {
